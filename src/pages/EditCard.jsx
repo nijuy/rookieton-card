266974e270d147bd5c81984b50c5f13f6e7e7981
nijuy <youjin6325@naver.com>
2023-05-20 19:19:41 +0900

    feat: 명함 편집 기능 (이름, 학과, 학번)

--- src/pages/EditCard.jsx
+++ src/pages/EditCard.jsx
@@ -1,18 +1,61 @@
 import styled from 'styled-components';
-import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+import { useRef } from 'react';
+import EditInput from '../components/EditInput';
 
 const EditCard = () => {
+  const nameRef = useRef();
+  const deptRef = useRef();
+  const idRef = useRef();
+
+  const navigate = useNavigate();
+
+  const emptyInputFocus = (targetRef) => {
+    targetRef.current.style.border = '1px solid red';
+    targetRef.current.focus();
+  };
+
+  const onClick = () => {
+    const name = nameRef.current.value;
+    const dept = deptRef.current.value;
+    const studentID = idRef.current.value;
+
+    if (name && dept && studentID) {
+      localStorage.setItem(
+        'card',
+        JSON.stringify({
+          name: name,
+          dept: dept,
+          studentID: studentID,
+          // mbti: 'INTP',
+          // alcohol: 1,
+        }),
+      );
+
+      navigate('/');
+    } else {
+      if (!name) emptyInputFocus(nameRef);
+      if (!dept) emptyInputFocus(deptRef);
+      if (!studentID) emptyInputFocus(idRef);
+    }
+  };
+
   return (
     <>
-      Card Edit Page
-      <Button to="/">완성하기</Button>
+      <EditInput target={'이름'} ref={nameRef} />
+      <EditInput target={'학과'} ref={deptRef} />
+      <EditInput target={'학번'} maxLength={2} ref={idRef} />
+      <div> mbti </div>
+      <div> 주량 </div>
+
+      <Button onClick={onClick}> 완성하기 </Button>
     </>
   );
 };
 
 export default EditCard;
 
-const Button = styled(Link)`
+const Button = styled.button`
   position: absolute;
   top: 50%;
   left: 50%;
