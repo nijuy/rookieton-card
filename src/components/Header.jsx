c36ba801fc7bd4bdb49cc500e621f393782cae7a
nijuy <youjin6325@naver.com>
2023-05-26 00:02:32 +0900

    feat(header): 컴포넌트 메뉴 기능 추가

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,6 +7,11 @@ import edit_button from '../assets/edit_button.svg';
 const Header = () => {
   const navigate = useNavigate();
   const [isShowEditButton, setIsShowEditButton] = useState(false);
+  const [openMenu, setOpenMenu] = useState(false);
+
+  const reverseOpenMenu = () => {
+    setOpenMenu((prev) => !prev);
+  };
 
   useEffect(() => {
     if (window.location.pathname === '/edit' || !localStorage.getItem('card')) {
@@ -18,8 +23,26 @@ const Header = () => {
 
   return (
     <>
+      {openMenu && (
+        <MenuContainer onClick={reverseOpenMenu}>
+          <MenuBox
+            onClick={(e) => {
+              e.stopPropagation();
+            }}
+          >
+            <button onClick={reverseOpenMenu}>X</button>
+            <ul>
+              <li> 서비스 설명 </li>
+              <li> 만든 사람 </li>
+              <li> 이용 약관 </li>
+              <li> 개인정보 처리방침 </li>
+            </ul>
+          </MenuBox>
+        </MenuContainer>
+      )}
+
       <ButtonBox>
-        <ImageButton>
+        <ImageButton onClick={reverseOpenMenu}>
           <img src={menu_button} alt="" />
         </ImageButton>
         {isShowEditButton && (
@@ -48,4 +71,22 @@ const ImageButton = styled.button`
   border: none;
   background-color: transparent;
   cursor: pointer;
+`;
+
+const MenuContainer = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  background-color: rgba(0, 0, 0, 0.4);
+  width: 100vw;
+  height: 100vh;
+`;
+
+const MenuBox = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  background-color: white;
+  height: 400px;
+  width: 250px;
 `;
