093db3052d2ce84e02e4e3ed2a4017e5b396ef73
nijuy <youjin6325@naver.com>
2023-05-26 00:18:51 +0900

    feat: 폰트 적용 (Noto Sans)

--- src/style/global.jsx
+++ src/style/global.jsx
@@ -1,5 +1,8 @@
 import { createGlobalStyle } from 'styled-components';
 
 export const GlobalStyle = createGlobalStyle`
-// 폰트 적용은 여기서
+* {
+    font-family: 'Noto Sans KR';
+    font-style: normal;
+}
 `;
