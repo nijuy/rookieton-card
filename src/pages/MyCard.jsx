35eb875ecb4b0b18c0032422087e55d10c8a77a4
nijuy <youjin6325@naver.com>
2023-05-26 19:46:49 +0900

    design: 명함 테두리, 띠지, 텍스트 로고 추가

--- src/pages/MyCard.jsx
+++ src/pages/MyCard.jsx
@@ -3,6 +3,7 @@ import { useState, useEffect } from 'react';
 import Button from '../components/Button';
 import Header from '../components/Header';
 import start_ppussung from '../assets/start_ppussung.svg';
+import yourssu_belt from '../assets/yourssu_belt.svg';
 
 const MyCard = () => {
   const [card, setCard] = useState({});
@@ -33,17 +34,22 @@ const MyCard = () => {
         </>
       ) : (
         <>
-          <Card>
-            {Object.keys(card).map((key) => (
-              <TextBox key={key}>
-                <Text> {key}&nbsp; </Text>
-                {key === 'Drink'
-                  ? `${parseInt(card[key])}병 ${(card[key] - parseInt(card[key])) * 8}잔`
-                  : card[key]}
-                {key === 'St_ID' && <>학번</>}
-              </TextBox>
-            ))}
-          </Card>
+          <div style={{ border: '1.5px solid #a3a3a3', borderRadius: '10px', padding: '10px' }}>
+            <Card>
+              ~ 뿌슝이 자리 ~
+              <Belt background={yourssu_belt} />
+              {Object.keys(card).map((key) => (
+                <TextBox key={key}>
+                  <Text> {key}&nbsp; </Text>
+                  {key === 'Drink'
+                    ? `${parseInt(card[key])}병 ${(card[key] - parseInt(card[key])) * 8}잔`
+                    : card[key]}
+                  {key === 'St_ID' && <>학번</>}
+                </TextBox>
+              ))}
+              ~ 술병 자리 ~<Text color="#A9E0FF"> Yourssu </Text>
+            </Card>
+          </div>
         </>
       )}
     </>
@@ -53,15 +59,30 @@ const MyCard = () => {
 export default MyCard;
 
 const Card = styled.div`
-  border: 1px solid;
+  border: 2px solid #a9e0ff;
+  border-radius: 10px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 `;
 
 const TextBox = styled.div`
   line-height: 3rem;
+  width: fit-content;
 `;
 
 const Text = styled.p`
   display: inline;
   font-weight: 500;
   font-size: 1rem;
+  color: ${(props) => (props.color ? props.color : 'black')};
+  font-style: ${(props) => (props.color ? 'italic' : 'normal')};
+`;
+
+const Belt = styled.div`
+  background-image: url(${(props) => props.background});
+  background-repeat: repeat-x;
+  width: 100%;
+  z-index: -1;
+  height: 30px;
 `;
