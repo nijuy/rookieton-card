b6521c53436fa2334d3361c3076fb0c367682459
nijuy <youjin6325@naver.com>
2023-05-27 23:38:04 +0900

    design(RadioSelector): mbti 버튼 간격, 크기 조절

--- src/components/RadioSelector.jsx
+++ src/components/RadioSelector.jsx
@@ -44,7 +44,7 @@ const RadioSelector = forwardRef((props, ref) => {
 });
 
 const Container = styled.div`
-  margin: 0px 35px 35px 0px;
+  margin: 0px 15px 15px 0px;
   display: flex;
   flex-direction: column;
   align-items: center;
@@ -54,7 +54,7 @@ const RadioInputLabel = styled.label`
   border: 1px solid #a3a3a3;
   border-right: ${(props) => props.direction === 'L' && 'none'};
   border-radius: ${(props) => (props.direction === 'L' ? '8px 0px 0px 8px' : '0px 8px 8px 0px')};
-  width: 80px;
+  width: 60px;
   height: 24px;
   display: flex;
   justify-content: center;
