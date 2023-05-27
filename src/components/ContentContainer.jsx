9640d414b81de6cad85dcae43a36c7413bd1c908
nijuy <youjin6325@naver.com>
2023-05-27 23:28:54 +0900

    design: ContentContainer 컴포넌트 생성

---
+++ src/components/ContentContainer.jsx
@@ -0,0 +1,17 @@
+import { styled } from 'styled-components';
+
+const ContentContainer = ({ children }) => {
+  return <Container>{children}</Container>;
+};
+
+const Container = styled.div`
+  border: 1px solid red;
+  height: 800px;
+  text-align: center;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+`;
+
+export default ContentContainer;
