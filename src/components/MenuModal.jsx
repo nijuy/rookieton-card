0156ae351a5c23f38efc64b3eac8fd2978cb05f8
nijuy <youjin6325@naver.com>
2023-05-28 15:16:51 +0900

    feat(MenuModal): MenuText 적용

--- src/components/MenuModal.jsx
+++ src/components/MenuModal.jsx
@@ -1,10 +1,19 @@
 import { styled } from 'styled-components';
 import close_button from '../assets/close_button.svg';
 import logo from '../assets/logo.svg';
+import { serviceIntro, serviceCreator, servicePolicy, serviceCaution } from '../assets/MenuText';
 
 const MenuModal = (props) => {
   const { title, onClickClose } = props;
-  const text = '세부 메뉴';
+
+  const setText = () => {
+    if (title === 'intro') return [serviceIntro];
+    if (title === 'creator') return [serviceCreator];
+    if (title === 'policy') return [servicePolicy];
+    if (title === 'warning') return [servicePolicy, serviceCaution];
+  };
+
+  const text = setText();
 
   return (
     <Container onClick={onClickClose}>
@@ -26,8 +35,9 @@ const MenuModal = (props) => {
             <img src={close_button} alt="" />
           </ImageButton>
         </div>
-        <br />
-        {text}
+        {text.map((textData, ind) => (
+          <TextContent key={ind} dangerouslySetInnerHTML={{ __html: textData }}></TextContent>
+        ))}
       </TextBox>
     </Container>
   );
@@ -45,7 +55,7 @@ const Container = styled.div`
 `;
 
 const TextBox = styled.div`
-  width: 312px;
+  width: 320px;
   height: 409px;
   box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
   border-radius: 20px;
@@ -53,6 +63,7 @@ const TextBox = styled.div`
   padding: 20px;
   display: flex;
   flex-direction: column;
+  overflow: scroll;
 
   div {
     display: flex;
@@ -69,6 +80,27 @@ const ImageButton = styled.button`
   background-color: transparent;
   cursor: pointer;
   height: fit-content;
+`;
+
+const TextContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  font-size: smaller;
+
+  h2 {
+    text-align: center;
+    margin-bottom: 0;
+  }
+
+  h4 {
+    margin-bottom: 0;
+    text-align: center;
+  }
+
+  h5 {
+    margin: 0;
+    text-align: center;
+  }
 `;
 
 export default MenuModal;
